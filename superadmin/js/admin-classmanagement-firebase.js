9051ad41cec12b197dfedfa0bdcbf24bea56a95d
Stighluck <encabo.sean@gmail,com>
2024-12-10 00:47:46 +0800

    classmanagement

---
+++ superadmin/js/admin-classmanagement-firebase.js
@@ -0,0 +1,59 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-app.js";
+import { getFirestore, updateDoc, getDoc, getDocs, doc, collection, getCountFromServer } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-firestore.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-auth.js";
+import { query, where } from "https://www.gstatic.com/firebasejs/11.0.0/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDQrgbw4TlLtLbex-BiEk58nA4l_zoDAmo",
+  authDomain: "bbba-96d01.firebaseapp.com",
+  projectId: "bbba-96d01",
+  storageBucket: "bbba-96d01.firebasestorage.app",
+  messagingSenderId: "9762028958",
+  appId: "1:9762028958:web:4b10cd0c5458b098e56a8f",
+  measurementId: "G-8YJS0ZTJ4C"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+onAuthStateChanged(auth, (user) => {
+  if (user) {
+    console.log(user);
+
+    const uid = user.uid;
+  } else {
+
+  }
+});
+
+const db = getFirestore(app);
+const gradeCollectionRef = collection(db, "gradeLevel");
+const gradeSnapshot = await getDocs(gradeCollectionRef);
+const teacherCollectionRef = collection(db, "teacher");
+const teacherSnapshot = await getDocs(teacherCollectionRef);
+
+let classDetails = [];
+const tableTemplate = document.querySelector('[user-template]');
+const tableCM = document.getElementById("tableCM");
+
+async function getClassDetails() {
+  try {
+    classDetails = gradeSnapshot.docs.map((doc) => {
+      const classes = tableTemplate.content.cloneNode(true).children[0];
+      classes.querySelector("#CMTeacher").innerHTML = doc.data()["teacher"];
+      classes.querySelector("#CMGrade").innerHTML = doc.data()["gradeLevel"];
+      classes.classList.remove("hidden");
+      tableCM.append(classes);
+      return {
+        studentID: doc.data()["studentID"] || "",
+        Name: doc.data()["lastName"] + ", " + doc.data()["firstName"] || "",
+        gradeLevel: doc.data()["gradeLevel"] || "",
+        element: student
+      };
+    });
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+}
+
+getClassDetails();
